746462122af9750fcd7c75278833240ece282b53
saadkhaan <junk@techie.com>
2021-08-17 11:03:57 +0500

    GA tag

--- pages/_document.js
+++ pages/_document.js
@@ -24,21 +24,21 @@ class MyDocument extends Document {
           <script
             async
             dangerouslySetInnerHTML={{
-              __html: `(function(c,l,a,r,i,t,y){
-                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                        })(window, document, "clarity", "script", "7yaxxd6y7w");`,
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-KXPZNDPMVE');`,
             }}
           />
           <script
             async
             dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', ${`G-KXPZNDPMVE`});`,
+              __html: `(function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "7yaxxd6y7w");`,
             }}
           />
         </Head>
