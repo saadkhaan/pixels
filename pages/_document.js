f73c7e5cbae2ae0ac42d3c0cd5fd26c203b71cd5
saadkhaan <junk@techie.com>
2021-09-02 09:18:19 +0500

    script tags updated

--- pages/_document.js
+++ pages/_document.js
@@ -47,7 +47,6 @@ class MyDocument extends Document {
 
           <script dangerouslySetInnerHTML={{
               __html: `<!-- VISA Tracking Code for pixelsbit.com -->
-              <script>
               (function(v,i,s,a){if(!v._visaSettings){v._visaSettings={};}v._visaSettings["8eb55897-0639-11ec-b589-901b0edac50a"]={v:"0.3",s:"8eb55897-0639-11ec-b589-901b0edac50a",a:"1"};_v=i.getElementsByTagName("body")[0];_a=_v;_i=i.createElement("script");_s=_i;_s.defer="defer";_s.src=s+a+v._visaSettings["8eb55897-0639-11ec-b589-901b0edac50a"].v;_a.appendChild(_s);})(window,document,"//app-worker.visitor-analytics.io/main",".js?s=8eb55897-0639-11ec-b589-901b0edac50a&v=")</script>
               <!-- VISA Tracking Code for pixelsbit.com -->`,
             }}></script>
