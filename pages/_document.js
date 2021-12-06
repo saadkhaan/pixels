f65ffc8c2ee27dd47c90b63320b349223184b47f
saadkhaan <saad@pixelsbit.com>
2021-12-06 10:53:06 +0500

    facebook domani verification

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+        <meta name="facebook-domain-verification" content="9hssa3778x6r0km0ds63pnc5syxu5z" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
